e6648ce7bbe4aaf0c37e3c6abe1e60459f138c89
Okazaki Kohei <marie673g@gmail.com>
2021-04-23 23:02:35 +0900

    cmake-build-debugの編集

--- cmake-build-debug/CMakeFiles/Programing.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Programing.dir/cmake_clean.cmake
@@ -2,6 +2,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/Programing.dir/01/kadai1.c.obj"
   "CMakeFiles/Programing.dir/01/kadai2.c.obj"
   "CMakeFiles/Programing.dir/01/kadai3.c.obj"
+  "CMakeFiles/Programing.dir/01/kadai4.c.obj"
   "CMakeFiles/Programing.dir/pre/kadai1.c.obj"
   "CMakeFiles/Programing.dir/pre/kadai2.c.obj"
   "CMakeFiles/Programing.dir/pre/kadai3.c.obj"
