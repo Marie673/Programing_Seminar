5c440fac374b97dd0a5c814a29cfa93f355ebf43
Okazaki Kohei <marie673g@gmail.com>
2021-04-23 21:24:01 +0900

    課題の読み間違いによる余剰機能を削除

--- 01/kadai1.c
+++ 01/kadai1.c
@@ -24,16 +24,8 @@ int main(int argc, char **argv){
     target_file_name = argv[2];
 
     original_fp = fopen(original_file_name, "r");
-    if(original_fp == 0){
-        fprintf(stderr, "Couldn't open the file: %s\n", original_file_name);
-        exit(1);
-    }
 
     target_fp = fopen(target_file_name, "w");
-    if(target_fp == 0){
-        fprintf(stderr, "Couldn't open the file: %s\n", target_file_name);
-        exit(1);
-    }
 
     int ch;
     while((ch = fgetc(original_fp)) != EOF){
