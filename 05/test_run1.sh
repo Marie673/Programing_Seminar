d5e9872b2e22054a1d1944e3d9476f0f316138cf
Okazaki Kohei <marie673g@gmail.com>
2021-06-02 14:35:41 +0900

    test run fix

--- 05/test_run1.sh
+++ 05/test_run1.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-./HTTP_client 165.242.111.77 80 > test_result_1.txt
+echo ./HTTP_client 165.242.111.77 80
+./HTTP_client 165.242.111.77 80
 # 2190 index.html
 
-./HTTP_client -v HTTP/1.0 -p /index.html  165.242.111.77 80 > test_result_2.txt
+echo ./HTTP_client -v HTTP/1.0 -p /index.html  165.242.111.77 80
+./HTTP_client -v HTTP/1.0 -p /index.html  165.242.111.77 80
 # 2190 index(1).html
 
-./HTTP_client -v HTTP/1.1 -p /index.html -p /members-j.html -h www.net.info.hiroshima-cu.ac.jp 165.242.111.77 80 > test_result_3.txt
+echo ./HTTP_client -v HTTP/1.1 -p /index.html -p /members-j.html -h www.net.info.hiroshima-cu.ac.jp 165.242.111.77 80
+./HTTP_client -v HTTP/1.1 -p /index.html -p /members-j.html -h www.net.info.hiroshima-cu.ac.jp 165.242.111.77 80
 # 2190 index(2).html
 # 5285 members.html
 
-./HTTP_client -v HTTP/1.0 -p /test.html  165.242.111.77 80 > test_result_4.txt
+echo ./HTTP_client -v HTTP/1.0 -p /test.html  165.242.111.77 80
+./HTTP_client -v HTTP/1.0 -p /test.html  165.242.111.77 80
 # Could not get Data
 
-./HTTP_client -v HTTP/1.1 -p /index.html 165.242.111.77 80 > test_result_5.txt
+echo ./HTTP_client -v HTTP/1.1 -p /index.html 165.242.111.77 80
+./HTTP_client -v HTTP/1.1 -p /index.html 165.242.111.77 80
 # HTTP/1.1 need Host
 
-./HTTP_client -v HTTP/1.0 -p /index.html http://www.net.info.hiroshima-cu.ac.jp 80 > test_result_6.txt
+echo ./HTTP_client -v HTTP/1.0 -p /index.html http://www.net.info.hiroshima-cu.ac.jp 80
+./HTTP_client -v HTTP/1.0 -p /index.html http://www.net.info.hiroshima-cu.ac.jp 80
 # 2190 index(3).html
